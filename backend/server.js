1dd2b589e3127ccc428a483c58ca28dd8e3aafe4
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2024-10-21 18:27:22 -0300

    sending and receiving messages

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,44 @@ const options = {
 
 const server = new WebSocketServer(options)
 
-const onlineUsers = []
+const connections = []
 
 
-server.on('connection', (connection, req) =>{
+server.on('connection', (ws, req) =>{
     const url = new URL(req.url, `http://${req.headers.host}`);
     const user = url.searchParams.get('user');
-    
+
+    connections.push({user, ws})
+
     console.log(`User connected ${user}`);   
-    connection.on('error', (error,)=>{
+    ws.on('error', (error,)=>{
         console.log(error);
     })
     
-    connection.on('message', (message) =>{
+    ws.on('message', (message) =>{
         console.log('Message received: ' + message.toString());
+        const parsedMessage =  JSON.parse(message.toString())
+        console.log(parsedMessage.action)
+
+        if (parsedMessage.action === 'get-online-users'){
+            ws.send(getOnlineUsersList())
+        }
+
+        if (parsedMessage.action === 'send-message'){
+            const foundConnection = connections.find(connection => connection.user === parsedMessage.receiver)
+
+            if (foundConnection){
+                foundConnection.ws.send(JSON.stringify({action: 'receive-message', message: parsedMessage.message}))
+            }
+        }
     })
     
-    connection.on('close', () =>{
+    ws.on('close', () =>{
         console.log('Connection closed');
     }) 
 })
+
+function getOnlineUsersList(){
+    return connections.map(connection => connection.user)
+}
 
